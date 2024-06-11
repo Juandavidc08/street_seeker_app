24dd3fc3d32c3dc2850f1400550cc59767e42b0d
Juan David Camelo <jdavidcamelo94@gmail.com>
2024-06-11 16:33:17 +0000

    Add home, street_search templates and views structure

--- street_seeker/views.py
+++ street_seeker/views.py
@@ -1,7 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Place
 
 # Create your views here.
 
 def home(request):
-    return HttpResponse("Hello, Steet Seeker!")
+    return render(request, "home.html")
+
+
+def places(request):
+    venue = Place.objects.all()
+    return render(request, "street_search.html", {"places": venue})
