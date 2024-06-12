b0603bc255efc0db94a91d029649bf26bc590950
Juan David Camelo <jdavidcamelo94@gmail.com>
2024-06-12 18:54:49 +0000

    Add places to database

--- street_seeker/models.py
+++ street_seeker/models.py
@@ -6,6 +6,7 @@ class Place(models.Model):
     name = models.CharField(max_length=200)
     description = models.TextField()
     location = models.CharField(max_length=200)
+    
 
     def __str__(self):
         return self.name
