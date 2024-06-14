7f4c06352dc3eff7342a624aec06e57be01db053
Juan David Camelo <jdavidcamelo94@gmail.com>
2024-06-14 18:30:16 +0000

    Add search button and create loop to show 3 places

--- street_seeker/views.py
+++ street_seeker/views.py
@@ -1,13 +1,17 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Place
+from random import sample
 
 # Create your views here.
 
 def home(request):
     return render(request, "home.html")
 
-
 def places(request):
-    venue = Place.objects.all()
-    return render(request, "street_search.html", {"places": venue})
+    all_places = list(Place.objects.all())
+    if len(all_places) >= 3:
+        selected_places = sample(all_places, 3)
+    else:
+        selected_places = all_places
+    return render(request, "street_search.html", {"places": selected_places})
